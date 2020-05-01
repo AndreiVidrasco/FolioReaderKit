3eb8c56c58ab4f9d22cdbac2d3ed2d337291fd95
Andrei Vidrasco <andrei.vidrasco@icloud.com>
2020-05-01 22:11:29 +0200

    Create as package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "FolioReaderKit",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v3)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
